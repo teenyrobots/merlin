a0e8236cdf2375e8fd8688b63a30682242195b8c
teenyrobots <hellojoycerice@gmail.com>
2016-10-31 14:39:58 -0400

    Initial positioning
    Get them comic pages started in a beautiful row

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 // javascript for Title TK
 
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
-
 var comicPages = [
 	"images/1.jpg",
 	"images/2.jpg",
@@ -22,3 +9,11 @@ var comicPages = [
 	"images/6.jpg",
 	"images/7.jpg"
 ]
+
+function nextPage() {
+
+}
+
+function prevPage() {
+	
+}
